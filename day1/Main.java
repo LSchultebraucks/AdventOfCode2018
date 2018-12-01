ac1392c508e15e4a75784bb8e854be662fa256e0
LSchultebraucks <L.Schultebraucks@t-online.de>
2018-12-01 12:15:39 +0100

    day1 Java partTwo

--- day1/Main.java
+++ day1/Main.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -16,8 +19,8 @@ public class Main {
             Scanner scanner = new Scanner(inputFile);
 
             while (scanner.hasNext()) {
-            int frequency = scanner.nextInt();
-            currentFrequency += frequency;
+                int frequency = scanner.nextInt();
+                currentFrequency += frequency;
             }
 
             scanner.close();
@@ -29,6 +32,43 @@ public class Main {
     }
 
     private static void partTwo() {
+        int currentFrequency = 0;
+
+        HashMap<Integer, Boolean> pastFrequences = new HashMap<>();
+
+        pastFrequences.put(0, true);
+
+        List<Integer> frequencies = new ArrayList<>();
+
+        File inputFile = new File("input.txt");
+        try {
+            Scanner scanner = new Scanner(inputFile);
+
+            while (scanner.hasNext()) {
+                int frequency = scanner.nextInt();
+                frequencies.add(frequency);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        boolean twiceFrequencyNotFound = true;
+
+        while (twiceFrequencyNotFound) {
+            for (int frequency : frequencies) {
+                currentFrequency += frequency;
+                boolean isPastFrequency = pastFrequences.containsKey(currentFrequency);
+                if (isPastFrequency) {
+                    System.out.println(currentFrequency + " appeared twice");
+                    twiceFrequencyNotFound = false;
+                    break;
+                } else {
+                    pastFrequences.put(currentFrequency, true);
+                }
+            }
+        }
+
 
     }
 }
